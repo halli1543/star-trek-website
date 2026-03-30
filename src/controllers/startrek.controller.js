63b84dd72872dc175e9d75b77e2394c1f35c7470
haraldur <halliottosson@gmail.com>
2026-03-30 14:08:54 +0000

    3.mars.2026

--- src/controllers/startrek.controller.js
+++ src/controllers/startrek.controller.js
@@ -1,17 +1,17 @@
-const shows = [
-    { title: 'The Original Series', year: '1966-1969', seasons: 3},
-    { title: 'The Next Generation', year: '1987-1994', seasons: 7},
-    { title: 'Deep Space Nine', year: '1993-1999', seasons: 7},
-    { title: 'Voyager', year: '1995-2001', seasons: 7},
-    { title: 'Enterprise', year: '2001-2005', seasons: 4},
-    { title: 'Discovery', year: '2017-2024', seasons: 5},
-    { title: 'Strange New Worlds', year: '2022-present', seasons: 3},
-];
+const shows = require('../data/shows');
 
-exports.getHomePage = (req, res) => {
-    res.render('index', { title: 'Welcome to Star Trek' });
+exports.getHomePage = (req, res, next) => {
+    try {
+        res.render('index', { title: 'Welcome to Star Trek' });
+    } catch (err) {
+        next(err)
+    }
 };
 
-exports.getShows = (req, res) => {
-    res.render('shows', { title: 'Star Trek Shows', shows });
+exports.getShows = (req, res, next) => {
+    try {
+        res.render('shows', { title: 'Star Trek Shows', shows});
+    } catch (err) {
+        next(err);
+    }
 };
---
+++ src/data/shows.js
@@ -0,0 +1,11 @@
+const shows = [
+    { title: 'The Original Series', year: '1966-1969', seasons: 3},
+    { title: 'The Next Generation', year: '1987-1994', seasons: 7},
+    { title: 'Deep Space Nine', year: '1993-1999', seasons: 7},
+    { title: 'Voyager', year: '1995-2001', seasons: 7},
+    { title: 'Enterprise', year: '2001-2005', seasons: 4},
+    { title: 'Discovery', year: '2017-2024', seasons: 5},
+    { title: 'Strange New Worlds', year: '2022-present', seasons: 3},
+];
+
+module.exports = shows
