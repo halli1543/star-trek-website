2135a661b9b2dcf84c45c3797b0e3e389cdf8af5
haraldur <halliottosson@gmail.com>
2026-04-28 12:46:35 +0000

    updated show.js file to be more readble and added descriptions to some shows

--- src/data/shows.js
+++ src/data/shows.js
@@ -1,11 +1,42 @@
 const shows = [
-    { id: 'tos', title: 'The Original Series', year: '1966-1969', seasons: 3},
-    { id: 'tng', title: 'The Next Generation', year: '1987-1994', seasons: 7},
-    { id: 'ds9', title: 'Deep Space Nine', year: '1993-1999', seasons: 7},
-    { id: 'voy', title: 'Voyager', year: '1995-2001', seasons: 7},
-    { id: 'ent', title: 'Enterprise', year: '2001-2005', seasons: 4},
-    { id: 'dis', title: 'Discovery', year: '2017-2024', seasons: 5},
-    { id: 'snw', title: 'Strange New Worlds', year: '2022-present', seasons: 3},
+    { id: 'tos', 
+        title: 'The Original Series', 
+        year: '1966-1969', 
+        seasons: 3,
+        description: 'captian kirk banging every allien he can find, scotty fixing something, and McCoy being racist to spock.'
+    },
+    { id: 'tng', 
+        title: 'The Next Generation', 
+        year: '1987-1994', 
+        seasons: 7,
+        description: 'picard saying something poetic, riker doing the maneuver, beverly fucking a candle ghost'
+    },
+    { id: 'ds9', 
+        title: 'Deep Space Nine', 
+        year: '1993-1999', 
+        seasons: 7,
+        description: 'sisko committing war crimes, obrien must suffer, and bashir having a bromance with garak'
+    },
+    { id: 'voy', 
+        title: 'Voyager', 
+        year: '1995-2001', 
+        seasons: 7
+    },
+    { id: 'ent', 
+        title: 'Enterprise', 
+        year: '2001-2005', 
+        seasons: 4
+    },
+    { id: 'dis', 
+        title: 'Discovery', 
+        year: '2017-2024', 
+        seasons: 5
+    },
+    { id: 'snw', title: 
+        'Strange New Worlds', 
+        year: '2022-present', 
+        seasons: 3
+    },
 ];
 
 module.exports = shows
