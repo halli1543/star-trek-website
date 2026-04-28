bc302ac6f5b24a853763f058a6e5fd935792a72b
haraldur <halliottosson@gmail.com>
2026-04-28 16:56:35 +0000

    added descriptions and captains to all shows, made character and episode data files

--- src/data/shows.js
+++ src/data/shows.js
@@ -1,41 +1,73 @@
 const shows = [
-    { id: 'tos', 
+    { 
+        id: 'tos', 
         title: 'The Original Series', 
         year: '1966-1969', 
         seasons: 3,
-        description: 'captian kirk banging every allien he can find, scotty fixing something, and McCoy being racist to spock.'
+        description: 'captian kirk banging every allien he can find, scotty fixing something, and McCoy being racist to spock.',
+        captain: 'James T. Kirk',
+        characters: require('./characters/tos'),
+        episodes: require('./episodes/tos')
     },
-    { id: 'tng', 
+    { 
+        id: 'tng', 
         title: 'The Next Generation', 
         year: '1987-1994', 
         seasons: 7,
-        description: 'picard saying something poetic, riker doing the maneuver, beverly fucking a candle ghost'
+        description: 'picard saying something poetic, riker doing the maneuver, and beverly fucking a candle ghost',
+        captain: 'Jean-Luc Picard',
+        characters: require('./characters/tng'),
+        episodes: require('./episodes/tng')
     },
-    { id: 'ds9', 
+    { 
+        id: 'ds9', 
         title: 'Deep Space Nine', 
         year: '1993-1999', 
         seasons: 7,
-        description: 'sisko committing war crimes, obrien must suffer, and bashir having a bromance with garak'
+        description: 'sisko committing war crimes, obrien must suffer, and bashir having a bromance with garak',
+        captain: 'Benjamin Sisko',
+        characters: require('./characters/ds9'),
+        episodes: require('./episodes/ds9')
     },
-    { id: 'voy', 
+    { 
+        id: 'voy', 
         title: 'Voyager', 
         year: '1995-2001', 
-        seasons: 7
+        seasons: 7,
+        description: 'Janeway drinking her tenth cup of coffe, Chakotay saying something spiritual, and kim being an ensing,',
+        captain: 'Kathryn Janeway',
+        characters: require('./characters/voy'),
+        episodes: require('./episodes/voy')
     },
-    { id: 'ent', 
+    { 
+        id: 'ent', 
         title: 'Enterprise', 
         year: '2001-2005', 
-        seasons: 4
+        seasons: 4.,
+        description: 'archer watching water polo, tpol complaining about humans, tucker being southern',
+        captain: 'Jonathan Archer',
+        characters: require('./characters/ent'),
+        episodes: require('./episodes/ent')
     },
-    { id: 'dis', 
+    { 
+        id: 'dis', 
         title: 'Discovery', 
         year: '2017-2024', 
-        seasons: 5
+        seasons: 5,
+        description: 'burnham being the same person every season, suru being the only good character.',
+        captain: 'Michael Burnham',
+        characters: require('./characters/dis'),
+        episodes: require('./episodes/dis')
     },
-    { id: 'snw', title: 
-        'Strange New Worlds', 
+    { 
+        id: 'snw', 
+        title: 'Strange New Worlds', 
         year: '2022-present', 
-        seasons: 3
+        seasons: 3,
+        description: 'pike having the best hair in star trek, MBenga being second best dad, spock banging chapel.',
+        captain: 'Christopher Pike',
+        characters: require('./characters/snw'),
+        episodes: require('./episodes/snw')
     },
 ];
 
